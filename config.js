66b8b42d63e374d44a9f5479682796893c1085eb
DominicDev <s0l0estudi4r@gmail.com>
2025-12-18 13:08:29 +0100

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = ' LYKKE LI';
+export const nameSongs = [
+	"STAND BY ME", /*1*/
+	"INTO MY ARMS", /*2*/
+	"LOVE HURTS", /*3*/
+	"⅃ƎTOH OИ", /*4*/
+	"YAWA OӘ T'ИOႧ UOY", /*5*/
+	"TЯAƎH ЯUOY OT YAWHӘIH", /*6*/
+	"ƧTЯUH YԳԳAH", /*7*/
+	"⅃ƎƧUOЯAƆ", /*8*/
+	"Ⴇ5", /*9*/
+	"ЯƎVO", /*10*/
+	"I & Ü", /*11*/
+	"HARD RAIN", /*12*/
+	"DEEP END", /*13*/
+	"TWO NIGHTS", /*14*/
+	"LAST PIECE", /*15*/
+	"JAGUARS IN THE AIR", /*16*/
+	"SEX MONEY FEELINGS DIE", /*17*/
+	"SO SAD SO SEXY", /*18*/
+	"BETTER ALONE", /*19*/
+	"BAD WOMAN", /*20*/
+]
